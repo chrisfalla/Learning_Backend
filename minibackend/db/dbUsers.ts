29133ab5b8278c9d3bee40ca08b5d402b35d4157
Chrisfalla <christoferfallavillarreal@.com>
2025-11-25 17:02:13 -0500

    create minibackend, structure, models and database

---
+++ minibackend/db/dbUsers.ts
@@ -0,0 +1,7 @@
+const users = [
+    { id: 1, name: "carlos", email: "car@gmail.com" },
+    { id: 2, name: "chris", email: "chris@gmail.com" },
+    { id: 3, name: "ana", email: "ana@gmail.com" },
+    { id: 4, name: "luci", email: "luci@gmail.com" },
+    { id: 5, name: "ali", email: "ali@gmail.com" }
+];
