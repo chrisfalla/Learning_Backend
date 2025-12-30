6352c25f211189a2a2d2e6adea9c57efa3818ef7
chrisfalla <christoferfallavillarreal@gmail.com>
2025-12-30 11:08:27 -0500

    refactor: update migration scripts to use English file names

--- src/database/scripts/rollback.ts
+++ src/database/scripts/rollback.ts
@@ -1,74 +1,74 @@
 import pool from '../config/database';
-import * as migration027 from '../migrations/027_create_notificaciones_table';
+import * as migration027 from '../migrations/027_create_notifications_table';
 import * as migration026 from '../migrations/026_create_wishlist_items_table';
 import * as migration025 from '../migrations/025_create_wishlists_table';
-import * as migration024 from '../migrations/024_create_resenas_table';
-import * as migration023 from '../migrations/023_create_pagos_table';
-import * as migration022 from '../migrations/022_create_facturas_table';
-import * as migration021 from '../migrations/021_create_envios_table';
-import * as migration020 from '../migrations/020_create_items_orden_table';
-import * as migration019 from '../migrations/019_create_ordenes_table';
-import * as migration018 from '../migrations/018_create_cupones_table';
-import * as migration017 from '../migrations/017_create_items_carrito_table';
-import * as migration016 from '../migrations/016_create_carritos_table';
-import * as migration015 from '../migrations/015_create_ordenes_proveedor_table';
-import * as migration014 from '../migrations/014_create_proveedores_table';
-import * as migration013 from '../migrations/013_create_inventario_table';
-import * as migration012 from '../migrations/012_create_imagenes_producto_table';
-import * as migration011 from '../migrations/011_create_variantes_atributos_table';
-import * as migration010 from '../migrations/010_create_variantes_producto_table';
-import * as migration009 from '../migrations/009_create_valores_atributo_table';
-import * as migration008 from '../migrations/008_create_atributos_producto_table';
-import * as migration007 from '../migrations/007_create_productos_table';
-import * as migration006 from '../migrations/006_create_marcas_table';
-import * as migration005 from '../migrations/005_create_categorias_table';
-import * as migration004 from '../migrations/004_create_direcciones_table';
-import * as migration003 from '../migrations/003_create_tokens_autenticacion_table';
-import * as migration002 from '../migrations/002_create_proveedores_autenticacion_table';
-import * as migration001 from '../migrations/001_create_usuarios_table';
+import * as migration024 from '../migrations/024_create_reviews_table';
+import * as migration023 from '../migrations/023_create_payments_table';
+import * as migration022 from '../migrations/022_create_invoices_table';
+import * as migration021 from '../migrations/021_create_shipments_table';
+import * as migration020 from '../migrations/020_create_order_items_table';
+import * as migration019 from '../migrations/019_create_orders_table';
+import * as migration018 from '../migrations/018_create_coupons_table';
+import * as migration017 from '../migrations/017_create_cart_items_table';
+import * as migration016 from '../migrations/016_create_carts_table';
+import * as migration015 from '../migrations/015_create_supplier_orders_table';
+import * as migration014 from '../migrations/014_create_suppliers_table';
+import * as migration013 from '../migrations/013_create_inventory_table';
+import * as migration012 from '../migrations/012_create_product_images_table';
+import * as migration011 from '../migrations/011_create_variant_attributes_table';
+import * as migration010 from '../migrations/010_create_product_variants_table';
+import * as migration009 from '../migrations/009_create_attribute_values_table';
+import * as migration008 from '../migrations/008_create_product_attributes_table';
+import * as migration007 from '../migrations/007_create_products_table';
+import * as migration006 from '../migrations/006_create_brands_table';
+import * as migration005 from '../migrations/005_create_categories_table';
+import * as migration004 from '../migrations/004_create_addresses_table';
+import * as migration003 from '../migrations/003_create_auth_tokens_table';
+import * as migration002 from '../migrations/002_create_auth_providers_table';
+import * as migration001 from '../migrations/001_create_users_table';
 
 const migrations = [
-  { name: '027_create_notificaciones_table', ...migration027 },
+  { name: '027_create_notifications_table', ...migration027 },
   { name: '026_create_wishlist_items_table', ...migration026 },
   { name: '025_create_wishlists_table', ...migration025 },
-  { name: '024_create_resenas_table', ...migration024 },
-  { name: '023_create_pagos_table', ...migration023 },
-  { name: '022_create_facturas_table', ...migration022 },
-  { name: '021_create_envios_table', ...migration021 },
-  { name: '020_create_items_orden_table', ...migration020 },
-  { name: '019_create_ordenes_table', ...migration019 },
-  { name: '018_create_cupones_table', ...migration018 },
-  { name: '017_create_items_carrito_table', ...migration017 },
-  { name: '016_create_carritos_table', ...migration016 },
-  { name: '015_create_ordenes_proveedor_table', ...migration015 },
-  { name: '014_create_proveedores_table', ...migration014 },
-  { name: '013_create_inventario_table', ...migration013 },
-  { name: '012_create_imagenes_producto_table', ...migration012 },
-  { name: '011_create_variantes_atributos_table', ...migration011 },
-  { name: '010_create_variantes_producto_table', ...migration010 },
-  { name: '009_create_valores_atributo_table', ...migration009 },
-  { name: '008_create_atributos_producto_table', ...migration008 },
-  { name: '007_create_productos_table', ...migration007 },
-  { name: '006_create_marcas_table', ...migration006 },
-  { name: '005_create_categorias_table', ...migration005 },
-  { name: '004_create_direcciones_table', ...migration004 },
-  { name: '003_create_tokens_autenticacion_table', ...migration003 },
-  { name: '002_create_proveedores_autenticacion_table', ...migration002 },
-  { name: '001_create_usuarios_table', ...migration001 },
+  { name: '024_create_reviews_table', ...migration024 },
+  { name: '023_create_payments_table', ...migration023 },
+  { name: '022_create_invoices_table', ...migration022 },
+  { name: '021_create_shipments_table', ...migration021 },
+  { name: '020_create_order_items_table', ...migration020 },
+  { name: '019_create_orders_table', ...migration019 },
+  { name: '018_create_coupons_table', ...migration018 },
+  { name: '017_create_cart_items_table', ...migration017 },
+  { name: '016_create_carts_table', ...migration016 },
+  { name: '015_create_supplier_orders_table', ...migration015 },
+  { name: '014_create_suppliers_table', ...migration014 },
+  { name: '013_create_inventory_table', ...migration013 },
+  { name: '012_create_product_images_table', ...migration012 },
+  { name: '011_create_variant_attributes_table', ...migration011 },
+  { name: '010_create_product_variants_table', ...migration010 },
+  { name: '009_create_attribute_values_table', ...migration009 },
+  { name: '008_create_product_attributes_table', ...migration008 },
+  { name: '007_create_products_table', ...migration007 },
+  { name: '006_create_brands_table', ...migration006 },
+  { name: '005_create_categories_table', ...migration005 },
+  { name: '004_create_addresses_table', ...migration004 },
+  { name: '003_create_auth_tokens_table', ...migration003 },
+  { name: '002_create_auth_providers_table', ...migration002 },
+  { name: '001_create_users_table', ...migration001 },
 ];
 
 export const rollbackMigrations = async () => {
-  console.log('🔄 Revirtiendo migraciones...\n');
+  console.log('🔄 Rolling back migrations...\n');
   
   try {
     for (const migration of migrations) {
-      console.log(`📝 Revirtiendo: ${migration.name}`);
+      console.log(`📝 Rolling back: ${migration.name}`);
       await migration.down();
     }
     
-    console.log('\n🎉 Todas las migraciones revertidas exitosamente');
+    console.log('\n🎉 All migrations rolled back successfully');
   } catch (error) {
-    console.error('\n❌ Error revirtiendo migraciones:', error);
+    console.error('\n❌ Error rolling back migrations:', error);
     throw error;
   } finally {
     await pool.end();
