b4566a0cdb178d4a2d911e6e8df4386582299720
chrisfalla <christoferfallavillarreal@gmail.com>
2026-01-09 13:04:07 -0500

    create file to generate and verify the token

--- src/shared/utils/security/jwt.ts
+++ src/shared/utils/security/jwt.ts
@@ -1,16 +1,33 @@
-import jwt from "jsonwebtoken"
+import jwt, { JwtPayload as JwtLibPayload } from "jsonwebtoken";
 
-const JWT_SECRET = process.env.JWT_SECRET!;
-const JWT_EXPIRES_IN = "15m";
+const _JWT_SECRET = process.env.JWT_SECRET;
+const _JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
 
-interface JwtPayload {
-    id: number;
-    email: string;
-    role: string;
+if (!_JWT_SECRET || !_JWT_EXPIRES_IN) {
+  throw new Error("JWT configuration missing");
+}
+
+const JWT_SECRET: string = _JWT_SECRET;
+const JWT_EXPIRES_IN: string = _JWT_EXPIRES_IN;
+
+export interface JwtPayload {
+  id: number;
+  email: string;
+  role: string;
 }
 
 export const generateAccessToken = (payload: JwtPayload): string => {
-    return jwt.sign(payload, JWT_SECRET, {
-        expiresIn: JWT_EXPIRES_IN,
-    });
-};     
+  return jwt.sign(payload, JWT_SECRET, {
+    expiresIn: JWT_EXPIRES_IN,
+  });
+};
+
+export const verifyAccessToken = (token: string): JwtPayload => {
+  const decoded = jwt.verify(token, JWT_SECRET) as JwtLibPayload;
+
+  return {
+    id: decoded.id as number,
+    email: decoded.email as string,
+    role: decoded.role as string,
+  };
+};
